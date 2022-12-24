178321ce90aa226d69d78f83b120409160318778
Jens Bech-Sørensen <jens@bechsor.no>
2022-12-24 11:18:27 +0100

    clean

--- pages/index.tsx
+++
@@ -1,9 +0,0 @@
-import type { NextPage } from "next";
-import Footer from "../components/Footer";
-import Header from "../components/Header";
-
-const Home: NextPage = () => {
-  return <div>hello, there </div>;
-};
-
-export default Home;
